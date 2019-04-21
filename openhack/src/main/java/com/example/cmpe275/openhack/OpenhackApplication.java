3febd806c19e9a562f0fa3ef4bfdf27999228049
darshilpk3 <darshilpk3@gmail.com>
2019-04-21 15:57:19 -0700

    Initial Configuration

---
+++ openhack/src/main/java/com/example/cmpe275/openhack/OpenhackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmpe275.openhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenhackApplication.class, args);
+	}
+
+}
