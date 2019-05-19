e56b6c3df4a2664951c33a60d7f778a28f993a15
saybee <sayaleebhusari@gmail.com>
2019-05-18 20:57:20 -0700

    Removed google Login

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -35,77 +35,78 @@ class Login extends Component {
     }
     this.verifyUser = this.verifyUser.bind(this);
   }
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      //firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccess: () => false
-    },
-    //Start it here 
-    credentialHelper: firebaseui.auth.CredentialHelper.NONE
-    //End it here 
-  }
-  componentDidMount = () => {
-    var values = {}
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-      //console.log("user", user)
-      //console.log("user.email ", user.email)
-      if(user!=null){
-      axios.defaults.withCredentials = true;
-      axios.get(`http://localhost:8080/getuser/${user.email}`)
-        .then(response => {
-          if (response.status == 200 && response.data.email !== user.email) {
-            values.name = user.displayName.substr(0, user.displayName.indexOf(" "))
-            values.lastname = user.displayName.substr(user.displayName.indexOf(" "))
-            values.imgurl = user.photoURL
-            values.screenName = user.displayName
-            values.email = user.email
-            if (user.email.includes("@sjsu.edu")) {
-              values.usertype = "admin";
-            }
-            else {
-              values.usertype = "user";
-            }
-            if (user.emailVerified) {
-              values.verified = "Y"
-            }
-            else {
-              values.verified = "N"
-            };
-            console.log("Send Axios ");
-            axios.defaults.withCredentials = true;
-            axios.post('http://localhost:8080/adduser', values)
-              .then(response => {
-                console.log("Status Code : ", response.status);
-                console.log("Here", JSON.stringify(response));
-                if (response.status === 200) {
-                  localStorage.setItem("userId", response.data.id);
-                  localStorage.setItem("userName", response.data.screenName);
-                  localStorage.setItem("userType", response.data.usertype);
-                  this.props.history.push("/home");
-                }
-                // else {
-                //     swal("Kindly Register again with correct data", "", "error");
-                // }
-              });
-          }
-          else {
-            localStorage.setItem("userId", response.data.id);
-            localStorage.setItem("userName", response.data.screenName);
-            localStorage.setItem("userType", response.data.usertype);
-            this.props.history.push("/home");
-          }
-        })
-      }
-    });
+  // uiConfig = {
+  //   signInFlow: "popup",
+  //   signInSuccessUrl: '/home',
+  //   signInOptions: [
+  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  //     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  //     //firebase.auth.EmailAuthProvider.PROVIDER_ID
+  //   ],
+  //   callbacks: {
+  //     signInSuccess: () => false
+  //   },
+  //   //Start it here 
+  //   credentialHelper: firebaseui.auth.CredentialHelper.NONE
+  //   //End it here 
+  // }
+  // handleSignInWithGoogle = () => {
+  //   var values = {}
+  //   firebase.auth().onAuthStateChanged(user => {
+  //     this.setState({ isSignedIn: !!user })
+  //     //console.log("user", user)
+  //     //console.log("user.email ", user.email)
+  //     if (user != null) {
+  //       axios.defaults.withCredentials = true;
+  //       axios.get(`http://localhost:8080/getuser/${user.email}`)
+  //         .then(response => {
+  //           if (response.status == 200 && response.data.email !== user.email) {
+  //             values.name = user.displayName.substr(0, user.displayName.indexOf(" "))
+  //             values.lastname = user.displayName.substr(user.displayName.indexOf(" "))
+  //             values.imageurl = user.photoURL
+  //             values.screenName = user.displayName
+  //             values.email = user.email
+  //             if (user.email.includes("@sjsu.edu")) {
+  //               values.usertype = "admin";
+  //             }
+  //             else {
+  //               values.usertype = "user";
+  //             }
+  //             if (user.emailVerified) {
+  //               values.verified = "Y"
+  //             }
+  //             else {
+  //               values.verified = "N"
+  //             };
+  //             console.log("Send Axios ");
+  //             axios.defaults.withCredentials = true;
+  //             axios.post('http://localhost:8080/adduser', values)
+  //               .then(response => {
+  //                 console.log("Status Code : ", response.status);
+  //                 console.log("Here Google SignIN", JSON.stringify(response));
+  //                 if (response.status === 200) {
+  //                   localStorage.setItem("userId", response.data.id);
+  //                   localStorage.setItem("userName", response.data.screenName);
+  //                   localStorage.setItem("userType", response.data.usertype);
+  //                   this.props.history.push("/home");
+  //                 }
+  //                 // else {
+  //                 //     swal("Kindly Register again with correct data", "", "error");
+  //                 // }
+  //               });
+  //           }
+  //           else {
+  //             localStorage.setItem("userId", response.data.id);
+  //             localStorage.setItem("userName", response.data.screenName);
+  //             localStorage.setItem("userType", response.data.usertype);
+  //             this.props.history.push("/home");
+  //           }
+  //         })
+  //     }
+  //   });
 
 
-  }
+  // }
 
   async verifyUser(email) {
     console.log("user Verified");
@@ -222,10 +223,11 @@ class Login extends Component {
               firebaseAuth={firebase.auth()}
             />
           )} */}
-        <StyledFirebaseAuth
+         {/* <StyledFirebaseAuth
           uiConfig={this.uiConfig}
           firebaseAuth={firebase.auth()}
-        />
+        // uiCallback={this.handleSignInWithGoogle.bind(this)}
+        /> */}
         <Form onSubmit={this.handleSubmit} className="login-form">
           <Form.Item>
             {getFieldDecorator('email', {
